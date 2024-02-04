47605c013d5321febd340ee6002cf64d28083154
Sebastian M. Vintoñuke <71727934+SebastianVintonuke@users.noreply.github.com>
2024-02-03 23:17:12 -0300

    G007 => Hotfix 3

--- src/components/intro/intro.tsx
+++ src/components/intro/intro.tsx
@@ -10,7 +10,7 @@ function Intro() {
 
     return (
         <div className="d-flex justify-content-center align-items-end w-100 h-100" onClick={() => onClick()}>
-            <img className="position-absolute character wobble-hor-bottom " src="src\assets\img\asuka_1.png"></img>
+            <img className="position-absolute character wobble-hor-bottom " src="/img/asuka_1.png"></img>
             <div className="text-background w-100 h-25 p-5">
                 <h6 className="fs-3">{ CONVERSATION[current].characterName }</h6>
                 <span className="fs-5">{ CONVERSATION[current].text }</span>
